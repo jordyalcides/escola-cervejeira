fac7e0f8fca09f15b0d5a1da2f73b1ee09d0a12e
Jordy Alcides <jordy.alcides@gmail.com>
2017-08-20 22:20:25 -0300

    -Alterações pequenas

--- blog/models.py
+++ blog/models.py
@@ -25,3 +25,28 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+# class Beer(models.Model):
+#     style = models.CharField(max_length=200)
+#     country = models.CharField(max_length=200)
+#     category = models.CharField(max_length=200)
+#     harmony = models.CharField(max_length=200)
+#     picture = models.ImageField(
+#             upload_to = settings.MEDIA_ROOT)
+#     description = models.TextField()
+#     created_date = models.DateTimeField(
+#             default=timezone.now)
+#     published_date = models.DateTimeField(
+#             blank=True, null=True)
+#
+#     @property
+#     def picture_url(self):
+#         if self.picture and hasattr(self.picture, 'url'):
+#             return self.picture.url
+#
+#     def publish(self):
+#         self.published_date = timezone.now()
+#         self.save()
+#
+#     def __str__(self):
+#         return self.title

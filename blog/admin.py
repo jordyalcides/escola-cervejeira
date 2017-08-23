0b5d687052ed5b00b041932e045986703b67ad21
acensjr <projetos.acens@gmail.com>
2017-08-23 13:32:16 -0300

    -Adicionado: Template de curso Modelo Curso

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Beer
+from .models import Post, Beer, Course
 
 admin.site.register(Post)
 admin.site.register(Beer)
+admin.site.register(Course)
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    # picture = models.ImageField(
-    #         upload_to = settings.MEDIA_ROOT)
     picture = models.ImageField(
             upload_to = "")
     text = models.TextField()
@@ -28,16 +27,41 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    # def full_url(self):
-    #     return settings.MEDIA_ROOT+"/"+ self.picture.url
 
 class Beer(models.Model):
     name = models.CharField(max_length=200)
-    price = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
     category = models.CharField(max_length=200)
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], default=0)
     picture = models.ImageField(
             upload_to = "")
     description = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    @property
+    def picture_url(self):
+        if self.picture and hasattr(self.picture, 'url'):
+            return self.picture.url
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    place = models.CharField(max_length=200)
+    picture = models.ImageField(
+            upload_to = "")
+    description = models.TextField()
+    start_date = models.DateTimeField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(

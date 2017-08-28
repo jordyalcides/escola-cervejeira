f6fddc05d80a45d32c7701c5afe36e29c9bcd550
Jordy Alcides <jordy.alcides@gmail.com>
2017-08-28 20:48:22 -0300

    -Correção das variáveis com acento no modelo e view

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,11 @@ from datetime import date
 
 class Post(models.Model):
     autor = models.ForeignKey('auth.User')
-    título = models.CharField(max_length=200)
-    imagem = models.ImageField(
-            upload_to = "post/")
+    titulo = models.CharField(max_length=200, verbose_name='título')
+    imagem = models.ImageField(upload_to = "post/")
     texto = models.TextField()
-    date_de_criação = models.DateTimeField(
-            default=timezone.now)
-    data_de_publicação = models.DateTimeField(
-            blank=True, null=True)
+    date_de_criacao = models.DateTimeField(default=timezone.now, verbose_name='data de criação')
+    data_de_publicacao = models.DateTimeField(blank=True, null=True, verbose_name='data de publicação')
 
     @property
     def imagem_url(self):
@@ -23,25 +20,22 @@ class Post(models.Model):
             return self.imagem.url
 
     def publish(self):
-        self.data_de_publicação = timezone.now()
+        self.data_de_publicacao = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.título
+        return self.titulo
 
 
 class Cerveja(models.Model):
     nome = models.CharField(max_length=200)
-    preço = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    preco = models.DecimalField(default=0, max_digits=5, decimal_places=2, verbose_name='preço')
     categoria = models.CharField(max_length=200)
-    avaliação = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
-    imagem = models.ImageField(
-            upload_to = "cervejas/")
-    descrição = models.TextField(max_length=1000)
-    data_de_criação = models.DateTimeField(
-            default=timezone.now)
-    data_de_publicação = models.DateTimeField(
-            blank=True, null=True)
+    avaliacao = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)], verbose_name='avaliação')
+    imagem = models.ImageField(upload_to = "cervejas/")
+    descricao = models.TextField(max_length=1000, verbose_name='descrição')
+    data_de_criacao = models.DateTimeField(default=timezone.now, verbose_name='data de criação')
+    data_de_publicacao = models.DateTimeField(blank=True, null=True, verbose_name='data de publicação')
 
     @property
     def imagem_url(self):
@@ -49,7 +43,7 @@ class Cerveja(models.Model):
             return self.imagem.url
 
     def publish(self):
-        self.data_de_publicação = timezone.now()
+        self.data_de_publicacao = timezone.now()
         self.save()
 
     def __str__(self):
@@ -58,22 +52,19 @@ class Cerveja(models.Model):
 
 class Curso(models.Model):
     nome = models.CharField(max_length=200, validators=[MinLengthValidator(2)])
-    preço = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    preco = models.DecimalField(max_digits=6, decimal_places=2, default=0, verbose_name='preço')
     local = models.CharField(max_length=100)
-    endereço = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200, verbose_name='endereço')
     cep = models.CharField(max_length=9, validators=[MinLengthValidator(9)])
     cidade = models.CharField(max_length=100)
-    imagem = models.ImageField(
-            upload_to = "cursos/")
-    descrição = models.TextField()
+    imagem = models.ImageField(upload_to = "cursos/")
+    descricao = models.TextField(verbose_name='descrição')
     organizador = models.CharField(max_length=50)
     email = models.CharField(max_length=200, default='contato@escolacervejeira.com.br')
-    data_de_início = models.DateTimeField()
-    duração = models.IntegerField(default=0)
-    data_de_criação = models.DateTimeField(
-            default=timezone.now)
-    data_de_publicação = models.DateTimeField(
-            blank=True, null=True)
+    data_de_inicio = models.DateTimeField(default=timezone.now, verbose_name='data de início')
+    duracao = models.IntegerField(default=0, verbose_name='duração')
+    data_de_criacao = models.DateTimeField(default=timezone.now, verbose_name='data de criação')
+    data_de_publicacao = models.DateTimeField(blank=True, null=True, verbose_name='data de publicação')
 
     @property
     def imagem_url(self):
@@ -82,10 +73,10 @@ class Curso(models.Model):
 
     @property
     def iniciou(self):
-        return timezone.now() > self.data_de_início
+        return timezone.now() > self.data_de_inicio
 
     def publish(self):
-        self.data_de_publicação = timezone.now()
+        self.data_de_publicacao = timezone.now()
         self.save()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 from .models import Post, Cerveja, Curso
 
 def index(request):
-    posts = Post.objects.filter(data_de_publicação__lte=timezone.now()).order_by('data_de_publicação')
-    curso = get_object_or_404(Curso)
+    posts = Post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
+    curso = Curso.objects.last()
     return render(request, 'index.html', {'posts': posts , 'curso': curso})
 
 def blog(request):
-    posts = Post.objects.filter(data_de_publicação__lte=timezone.now()).order_by('data_de_publicação')
+    posts = Post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
     return render(request, 'blog/blog-with-sidebar.html', {'posts': posts})
 
 def blog_post(request, pk):
@@ -16,7 +16,7 @@ def blog_post(request, pk):
     return render(request, 'blog/blog_post.html', {'post': post})
 
 def cervejas(request):
-    cervejas = Cerveja.objects.filter(data_de_publicação__lte=timezone.now()).order_by('nome')
+    cervejas = Cerveja.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('nome')
     return render(request, 'blog/features-beer-shop.html', {'cervejas': cervejas})
 
 def cerveja(request, pk):
@@ -24,7 +24,7 @@ def cerveja(request, pk):
     return render(request, 'blog/features-beer-shop-product.html', {'cerveja': cerveja})
 
 def cursos(request):
-    cursos = Curso.objects.filter(data_de_publicação__lte=timezone.now()).order_by('nome')
+    cursos = Curso.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('nome')
     return render(request, 'blog/event-list.html', {'cursos': cursos})
 
 def curso(request, pk):

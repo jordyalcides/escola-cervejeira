af9fe769524dd6148c698ee7ee74d8309a821665
Gleidson Silva <gleidson.fgs@gmail.com>
2017-09-18 17:04:46 -0300

    alterando subtitulos

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,9 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
+admin.site.site_title = 'Escola Cervejeira'
 
+admin.site.site_header = 'Acens admin'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),

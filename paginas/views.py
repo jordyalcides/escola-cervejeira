cbdf5762f585109fa6aade811d4e92c4e01f2d82
Jordy Alcides <jordy.alcides@gmail.com>
2017-09-26 14:48:23 -0300

    -Atualização das views Paginas

--- paginas/views.py
+++ paginas/views.py
@@ -5,17 +5,17 @@ from .models import Contato, Equipe, Membro, Quem, Foto
 
 def quem(request):
     quem = Quem.objects.last()
-    return render(request, 'features-who.html', {'quem': quem})
+    return render(request, 'who.html', {'quem': quem})
 
 def equipe(request):
     contato = Contato.objects.last()
     equipe = Equipe.objects.last()
     membros = Membro.objects.all()
-    return render(request, 'features-staff.html', {'contato': contato, 'equipe': equipe, 'membros': membros})
+    return render(request, 'staff.html', {'contato': contato, 'equipe': equipe, 'membros': membros})
 
 def membro(request, pk):
     membro = get_object_or_404(Membro, pk=pk)
-    return render(request, 'features-staff-member-profile.html', {'membro': membro})
+    return render(request, 'member.html', {'membro': membro})
 
 def galeria(request):
     fotos = Foto.objects.all()
